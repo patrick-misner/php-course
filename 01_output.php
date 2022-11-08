6338e4a54f97c00e4fc4df9b8d8182a6ee44f131
Patrick Misner <misner.patrick@gmail.com>
2022-11-08 14:05:17 -0700

    pause

---
+++ 01_output.php
@@ -0,0 +1,29 @@
+<?php
+//echo 'Hello 3';
+//echo 'go away';
+//echo 123, 'hello', 10.5;
+
+// print_r([1, 2, 3]);
+var_dump('Hello');
+/* comments
+echo 1;
+echo 2;
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1><?php echo 'Post One' ?> </h1>
+  <h1><?= 'Post Two' ?> </h1>
+</body>
+
+</html>
